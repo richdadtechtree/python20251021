d7229093d487fac59b2a03d426267f8c89ac1d95
Richdadtechtree <bbonoyo@gmail.com>
2025-10-21 10:51:28 +0900

    클래서 연습중

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,6 @@
+
+# 부모 클래스 정의
+
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,17 +9,26 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+
+#자식 클래스 정의
 class Student(Person):
+    #상속받고 덮어쓰기
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #super() 함수를 사용
+        super().__init__(name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #덮어쓰기
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(학과:{0}, 학번: {1})".format(self.subject, self.studentID))        
 
 
+#인스턴스 생성
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+
+p.printInfo()
+s.printInfo()
 
 
